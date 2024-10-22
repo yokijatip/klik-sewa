98ac137595268991782b8502f9105febe533283b
Yoki Jati Perkasa <yokijati@gmail.com>
2024-10-22 18:58:02 +0700

    Success create item

---
+++ app/src/main/java/com/sinergi5/kliksewa/ui/cart/CartActivity.kt
@@ -0,0 +1,28 @@
+package com.sinergi5.kliksewa.ui.cart
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sinergi5.kliksewa.R
+import com.sinergi5.kliksewa.databinding.ActivityCartBinding
+
+class CartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityCartBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
